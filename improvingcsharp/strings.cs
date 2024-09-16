10b0e619da655aa3c9e6261a4a73b0237001faa1
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-09-16 17:52:49 -0300

    Add project files.

---
+++ improvingcsharp/strings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace improvingcsharp
+{
+    internal class strings
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Gustavo";
+            string sobreNome = "Roldão";
+            Console.WriteLine(string.Concat(nome, sobreNome));
+
+            // INTERPOLAÇÃO DE STRINGS
+            Console.WriteLine($"{nome} {sobreNome}");
+        }
+    }
+}
