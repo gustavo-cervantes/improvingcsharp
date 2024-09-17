a17b7c20c6a141ad4d83bd6529df2636ebb9a6c4
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-09-17 15:45:36 -0300

    improving C# day 2 booleans

--- Booleans/Program.cs
+++ Booleans/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Booleans
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isTrue = true;
+            bool isFalse = false;
+
+            int a = 10;
+            int b = 20;
+
+            // EXEMPLOS BOOLEANOS
+
+            Console.WriteLine(isTrue);
+            Console.WriteLine(isFalse);
+            Console.WriteLine(a > b);
+            Console.Write(a > 50);
+            Console.WriteLine(b == 20);
+            Console.WriteLine(20 == 20);
+        }
+    }
+}
