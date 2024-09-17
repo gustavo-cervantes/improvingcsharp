a1bd89002a44ccd30eec554b7ce18bb336e3610b
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-09-16 23:18:29 -0300

    improving C# day 1

--- improvingcsharp/strings.cs
+++ improvingcsharp/strings.cs
@@ -16,6 +16,19 @@ namespace improvingcsharp
 
             // INTERPOLAÇÃO DE STRINGS
             Console.WriteLine($"{nome} {sobreNome}");
+            
+            string nomeCompleto = $"{nome} {sobreNome}";
+            Console.WriteLine(nomeCompleto);
+            int posicao = nomeCompleto.IndexOf("G");
+            Console.WriteLine(posicao);
+
+            // PEGAR O SOBRENOME A PARTIR DA STRING
+
+            string nomeFinal = nomeCompleto.Substring(posicao);
+            Console.WriteLine(nomeFinal);
+            Console.WriteLine(nomeCompleto.ToUpper());
+            Console.WriteLine(nomeCompleto.ToLower());
+
         }
     }
 }
