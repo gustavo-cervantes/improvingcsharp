7ec34d584676059a58907fc9561712920fe8720b
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-09-18 17:24:06 -0300

    improving C# day3 switch case

---
+++ Switch/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Switch
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia = 8;
+            switch (dia)
+            {
+                case 1:
+                    Console.WriteLine("Domingo");
+                    break;
+                case 2:
+                    Console.WriteLine("Segunda-feira");
+                    break;
+                case 3:
+                    Console.WriteLine("Terça-feira");
+                    break;
+                case 4:
+                    Console.WriteLine("Quarta-feira");
+                    break;
+                case 5:
+                    Console.WriteLine("Quinta-feira");
+                    break;
+                case 6:
+                    Console.WriteLine("Sexta-feira");
+                    break;
+                case 7:
+                    Console.WriteLine("Sábado");
+                    break;
+
+                default: // NÃO OBRIGATORIO
+                    Console.WriteLine("Dia inválido");
+                    break;
+            }
+        }
+    }
+
+}
+
+
