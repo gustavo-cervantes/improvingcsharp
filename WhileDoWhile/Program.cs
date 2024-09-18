3f0000d2bc97230f2323abe8d5d9214c07505e33
Gustavo da Silveira Roldão <gustavodsroldao@gmail.com>
2024-09-18 17:58:33 -0300

    improving c# day 3 while do while

---
+++ WhileDoWhile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WhileDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // LOOPS SÃO UTILIZADOS PARA REPETIR UM BLOCO DE CÓDIGO VÁRIAS VEZES,
+            // ECONOMIZAR TEMPO, REDUZIR ERROS E DEIXAR CÓDIGOS MAIS LEGIVEIS.
+
+
+            // WHILE
+            // ENQUANTO FOR VERDADEIRO, EXECUTA O BLOCO DE CÓDIGO
+
+            int i = 0; // variavel
+
+            while (i < 10) 
+            {
+                Console.WriteLine(i);
+                i++; // INCREMENTA O VALOR DE i ATÉ QUE ELE SE COMPLETE ( CHEGUE EM 10 ) 
+            }
+
+            // DO WHILE,
+            // MESMA FUNÇÃO, PORÉM O BLOCO DE CÓDIGO SERÁ VERIFICADO POR ÚLTIMO
+            // ELE EXECUTA O BLOCO DE CÓDIGO PRIMEIRO E DEPOIS VERIFICA.
+
+            int j = 0; // variavel
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            } while (j < 20);
+        }
+    }
+}
